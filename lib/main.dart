0a9aaecdc3c810016aceaa05def6ec0c42a0ee46
lucasfrederico <lucas.frederico@outlook.com.br>
2020-04-05 15:57:03 -0300

    feat: finalização do projeto

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:buscadorgifs/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(hintColor: Colors.white)
+    ),
+  );
+}
